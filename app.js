7f959c3ac3a805b3f0508a32a814c43bd14490fc
Julian Correa <juacorreagi94@gmail.com>
2021-05-13 19:22:44 -0500

    Saludame 2

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+app.get('/makers/:name', (req, res) => {  
+
+    
+      const capitaliceWord = capitalizar(req.params.name);
+      res.send(`<h1>Hola ${capitaliceWord}!</h1>`);
+  
 });
+
+function capitalizar(str) {    
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();  
+}
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
